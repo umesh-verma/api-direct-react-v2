2437b158af361b430f9a0ac13ac35457ed4e32d2
Nischay Chandra <chandranischay@gmail.com>
2023-01-14 14:57:58 +0530

    card title adjustment

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:5000';
-// export const baseUrl = 'http://apidirect.healthcare:5000';
+// export const baseUrl = 'http://localhost:5000';
+export const baseUrl = 'http://apidirect.healthcare:5000';
