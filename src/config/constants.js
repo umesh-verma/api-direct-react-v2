25857aef99bdba2752e132fdb64746be89aefc09
Nischay Chandra <chandranischay@gmail.com>
2023-01-16 20:27:10 +0530

    https on api url

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
 // export const baseUrl = 'http://localhost:5000';
-export const baseUrl = 'http://apidirect.healthcare:5000';
+export const baseUrl =  window.location.href.startsWith('https') ? 'https://apidirect.healthcare:5000' : 'http://apidirect.healthcare:5000' ;
