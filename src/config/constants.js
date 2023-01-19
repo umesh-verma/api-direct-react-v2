e76353825866b596f20149abbe088d399da6a3c0
Umesh Verma <umesh.verma236@gmail.com>
2023-01-19 16:18:53 +0530

    changed port

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 // export const baseUrl = 'http://localhost:5000';
-export const baseUrl =  window.location.href.startsWith('https') ? 'https://apidirect.healthcare:5000' : 'http://apidirect.healthcare:5000' ;
+export const baseUrl =  window.location.href.startsWith('https') ? 'https://apidirect.healthcare:8443' : 'http://apidirect.healthcare:5000' ;
 // export const baseUrl = 'http://apidirect.healthcare:5000'
